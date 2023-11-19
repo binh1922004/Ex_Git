b49811022d11e12b9bd1c2ff623b88cb4adb9050
binh1922004 <binh1920042018@gmail.com>
2023-11-19 20:44:18 +0700

    add class SV

---
+++ Test/Test/SinhVien.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    internal class SinhVien
+    {
+        private string Name;
+        private string MSSV;
+        public(string Name, string MSSV);
+    }
+}
